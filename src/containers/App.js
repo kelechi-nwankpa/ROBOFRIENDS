e9facfc8e799b3356217054a27c06b7dca725532
nwankpa-D-boy <nwankpakelechisamuel@gmail.com>
2023-11-14 06:23:05 +0000

    added some  hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
+function App() {
+   
+    const [ robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
 
-
-    componentDidMount() {
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 return response.json();
             })
-            .then(users => this.setState({ robots: users }))
+            .then(users => setRobots(users))
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value );
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
+    
         const filteredRobots = robots.filter((robot) => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
@@ -38,9 +32,9 @@ class App extends Component {
             :
             (
                 <div className='tc'>
-                    <h1>RoboFriends</h1>
+                    <h1 className='f1'>RoboFriends</h1>
                     <SearchBox
-                        searchChange={this.onSearchChange}
+                        searchChange={onSearchChange}
                     />
                     <Scroll>
                         <ErrorBoundary>
@@ -50,6 +44,6 @@ class App extends Component {
                 </div>
             )
     }
-}
+
 
 export default App;
